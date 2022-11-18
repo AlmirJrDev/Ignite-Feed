9f7b14ba72f6a6c838897b7706b5122c1a307d5a
AlmirJrDev <almagomes1@hotmail.com>
2022-11-18 10:44:17 -0300

    keys

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,7 @@ function App() {
         {posts.map(post => {
           return (
           <Post 
+            key={post.id}
             author={post.author}
             content={post.content}
             publishedAt={post.publishedAt}
