4c36ad9a8483692fdb5f3db2687f711e02260db8
AlmirJrDev <almagomes1@hotmail.com>
2022-11-18 11:27:25 -0300

    botton opacity

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,7 @@ export function Post({author, publishedAt, content}) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');  
         setNewCommentText(event.target.value);
     }
 
@@ -40,6 +41,11 @@ export function Post({author, publishedAt, content}) {
         console.log(`deletar comentario ${comment}`)
     }
 
+    function handleNewCommentInvalid(){
+        event.target.setCustomValidity('Tá vendo esse campo aqui, ele é que nem sua namorada, não tem nada :)')
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0
     return (
         <article className={styles.post}>
             <header>
@@ -72,10 +78,12 @@ export function Post({author, publishedAt, content}) {
                  value={newCommentText}
                  placeholder='Deixe um comentário'
                  onChange={handleNewCommentChange}
+                 onInvalid={handleNewCommentInvalid}
+                 required
                 /> 
 
                 <footer>
-                  <button type='submit'>Publicar</button> 
+                  <button type='submit' disabled={isNewCommentEmpty}>Publicar</button> 
                 </footer>
             </form>
 
